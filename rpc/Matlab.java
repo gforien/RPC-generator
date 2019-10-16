33458a9a3a1e98d0df852eb5a9b962292f456751
Gabriel Forien <gforien@gmail.com>
2019-10-16 08:25:53 +0200

    v1 : version locale uniquement

---
+++ rpc/Matlab.java
@@ -0,0 +1,13 @@
+package rpc;
+
+public class Matlab {
+  private int i;
+
+  public Matlab(int i) {
+    this.i = i;
+  }
+
+  public Result calcul(int in) {
+    return new Result(in * this.i);
+  }
+}
